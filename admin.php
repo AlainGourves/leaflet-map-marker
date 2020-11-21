577377bee0aead7f127309cdd7ea2376eaa72b22
Alain Gourvès <alain.gourves@me.com>
2020-11-21 12:24:36 +0100

    initial commit

---
+++ admin.php
@@ -0,0 +1,120 @@
+<?php
+// Chargement fichier geoJson
+$url = __DIR__.'/js/salles.json';
+$json = json_decode(file_get_contents($url, true)); // convert JSON into PHP Assoc Array
+
+// Traitement du FORM --------------------------------
+function sanitizeString($var) {
+    $var = stripslashes($var);
+    $var = htmlentities($var);
+    $var = strip_tags($var);
+    return $var;
+}
+
+function alerte($message){
+    return "<span class=\"alerte\">". $message. "<span>";
+}
+
+function saveGeoJson($url, $json){
+    $jsonFile = fopen($url, "w");
+    fputs($jsonFile, json_encode($json));
+    fclose($jsonFile);
+}
+
+if (isset($_POST['submit'])) {
+    // Verif 1 : les champs ne sont pas vides
+    if (empty($_POST['nomSalle'])) {
+        $message['nomSalle'] = "Saisir un nom.";
+    } elseif (empty($_POST['adresseSalle'])) {
+        $message['adresseSalle'] = "Saisir une adresse.";
+    } elseif (empty($_POST['posSalle'])) {
+        $message['posSalle'] = "Placer un marqueur sur la carte.";
+    } elseif (!strlen(trim($_POST['sportsSalle']))) {
+        $message['sportsSalle'] = "Saisir le(s) sport(s) pratiqué(s).";
+    }else{
+        $feature = new stdClass; // objet anonyme
+        $properties = new stdClass; // objet anonyme
+        $geometry = new stdClass; // objet anonyme
+        $geometry->type = "Point";
+        $feature->type = "Feature";
+
+        $properties->name = sanitizeString($_POST['nomSalle']);
+        $properties->address = sanitizeString($_POST['adresseSalle']);
+        $tmp = preg_split("/(\s?,\s?)|(\n|\r)/", sanitizeString($_POST['sportsSalle']));
+        $sports = [];
+        foreach($tmp as $sport){
+            if($sport != "") $sports[] = trim($sport);
+        }
+        $properties->sports = $sports;
+        $position = explode(",", sanitizeString($_POST['posSalle']));
+        foreach($position as &$pos) {
+            $pos = (float)$pos;
+        }
+        $geometry->coordinates = [$position[1], $position[0]];
+        $feature->properties = $properties;
+        $feature->geometry = $geometry;
+
+        // MàJ du fichier geoJson
+        if(isset($_POST['idSalle']) && is_int((int)$_POST['idSalle'])){
+            // modif d'un feature
+            $json->features[$_POST['idSalle']] = $feature;
+        }else{
+            $json->features[] = $feature;
+        }
+
+        saveGeoJson($url, $json);
+    }
+}
+
+if (isset($_GET['action']) && ($_GET['action']=="modifier" || $_GET['action']=="supprimer")) {
+    $features = &$json->features; // & : passe par réf. pour que $json soit bien modifié
+    $id = $_GET['id'];
+    $name = urldecode($_GET['name']);
+    if (isset($features[$id])){
+        if ($name == $features[$id]->properties->name){
+
+            switch ($_GET['action']) {
+                case 'modifier':
+                    $modif['idSalle'] = $id;
+                    $modif['nomSalle'] = $features[$id]->properties->name;
+                    $modif['adresseSalle'] = $features[$id]->properties->address;
+                    $modif['posSalle'] = $features[$id]->geometry->coordinates;
+                    $modif['sportsSalle'] = implode("\n", $features[$id]->properties->sports);
+                break;
+                
+                case 'supprimer':
+                    unset($features[$id]);
+                    $features = array_values($features);// reset keys (sinon ça merde)
+                break;
+                    
+                default:
+                break;
+            }
+            saveGeoJson($url, $json);
+        }
+    }
+}
+
+
+// Calcul affichage --------------------------------
+$listeSalles = "";
+// Boucle sur les features
+$i = 0;
+foreach ($json->features as $feature) {
+    $listeSalles .= "<article class=\"salle\">\n";
+    $listeSalles .= "<header>\n";
+    $listeSalles .= "<a href=\"?action=modifier&amp;name=". urlencode($feature->properties->name). "&amp;id=". $i. "\" data-name=\"". $feature->properties->name. "\" class=\"btn\">Modif.</a>\n";
+    $listeSalles .= "<a href=\"?action=supprimer&amp;name=". urlencode($feature->properties->name). "&amp;id=". $i. "\" data-name=\"". $feature->properties->name. "\" class=\"btn\">Suppr.</a>\n";
+    $listeSalles .= "</header>\n";
+    $listeSalles .= "<h1>". $feature->properties->name. "</h1>\n";
+    $listeSalles .= "<p>". $feature->properties->address. "</p>\n";
+    $listeSalles .= "<p>";
+    foreach ($feature->properties->sports as $sport){
+        $listeSalles .= "<span class=\"btn\">". $sport. "</span>";
+    }
+    $listeSalles .= "</p>\n";
+    $listeSalles .= "</article>\n";
+    $i++;
+}
+
+include_once("admin.html");
---
+++ js/app.js
@@ -0,0 +1,148 @@
+const formSalle = document.querySelector("#ajoutSalles form");
+const nomSalle = document.querySelector("#nomSalle");
+const adresseSalle = document.querySelector("#adresseSalle");
+const posSalle = document.querySelector("#posSalle");
+const sportsSalle = document.querySelector("#sportsSalle");
+
+// Dialogue Confirm
+const dialogue = document.querySelector(".overlay");
+const dialogueOui = document.querySelector('#ouiSalle');
+const dialogueNon = document.querySelector('#nonSalle');
+
+const url = "js/salles.json";
+
+let mapSalles, myMap, marker;
+
+const attribution = "fond de carte par&nbsp;<a href=\"http://www.openstreetmap.fr/mentions-legales/\" target=\"_blank\" rel=\"nofollow noopener\" data-saferedirecturl=\"https://www.google.com/url?q=http://www.openstreetmap.fr/mentions-legales/&amp;source=gmail&amp;ust=1549536187967000&amp;usg=AFQjCNFnpX0mkyom6on-dpH6CUoxPBmVvQ\">OpenStreetMap France</a>, sous&nbsp;<a href=\"http://creativecommons.org/licenses/by-sa/2.0/fr/\" target=\"_blank\" rel=\"nofollow noopener\" data-saferedirecturl=\"https://www.google.com/url?q=http://creativecommons.org/licenses/by-sa/2.0/fr/&amp;source=gmail&amp;ust=1549536187967000&amp;usg=AFQjCNGylyk2k1uD6Cjh4C5kjLYM9ADqdw\">licence CC BY-SA</a>&nbsp;»";
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+
+// fonctions pour la carte des salles
+function displayInfoSalle(e) {
+    let layer = e.target;
+    let props = layer.feature.properties;
+    let content = '<h4>'+ props.name +'</h4>';
+    content += '<p>' + props.address + '</p>'
+    content += '<p>';
+    props.sports.forEach(e => {
+        content += '<span class="btn">' + e + '</span>'
+    });
+    content += '</p>';
+    layer.bindPopup(content, {closeButton: false});
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        click: displayInfoSalle
+    });
+}
+
+// fonctions pour l'ajout de salles
+function zoomTo(map, pos){
+    let markerBounds = L.latLngBounds([pos]);
+    map.fitBounds(markerBounds, {maxZoom: 15});
+}
+
+function onMapLoad(){
+    if(typeof markerOldPos !== 'undefined'){
+        // cas d'une modif
+        marker.setLatLng(markerOldPos);
+        zoomTo(myMap, markerOldPos);
+    }
+}
+
+function position(pos){
+    posSalle.value = pos.lat + ', ' + pos.lng;
+    posSalle.dataset.lat = pos.lat;
+    posSalle.dataset.lng = pos.lng;
+    getAddress(pos);
+}
+
+async function getAddress(pos){
+    const urlGeoAPI = `https://api-adresse.data.gouv.fr/reverse/?lon=${pos.lng}&lat=${pos.lat}`;
+    const response = await fetch(urlGeoAPI);
+    const data = await response.json();
+    adresseSalle.value = data.features[0].properties.name;
+}
+
+function reponseDialogue(){
+    return new Promise(function(resolve, reject){
+        dialogueOui.addEventListener('click', function(){
+            resolve();
+        });
+        dialogueNon.addEventListener('click', function(){
+            reject();
+        })
+    });
+}
+
+window.addEventListener("load", e => {
+    async function getData(url){
+        const response = await fetch(url);
+        const data = await response.json();
+        L.geoJson(data, {
+            onEachFeature: onEachFeature
+        }).addTo(mapSalles);
+    }
+
+    mapSalles = L.map("mapSalles").setView([48.087, -1.66], 15);
+
+    L.tileLayer(tileUrl, { attribution }).addTo(mapSalles);
+    const scale = L.control.scale({
+        metric: true,
+        imperial: false
+    })
+    scale.addTo(mapSalles);
+
+    getData(url);
+
+    // Ajout de salles
+    myMap = L.map("mapAjout");
+    L.tileLayer(tileUrl, { attribution }).addTo(myMap);
+
+    let legend = L.control({position: 'topright'});
+    legend.onAdd = function (map) {
+        let div = L.DomUtil.create('div', 'legende');
+        div.innerHTML = `<h4>Cliquez sur la carte pour placer un marqueur</h4>
+                    <p>Vous pouvez ensuite le déplacer à volonté</p>`;
+        return div;
+    };
+    legend.addTo(myMap);
+    marker = L.marker([0,0], { draggable: true}).addTo(myMap);
+
+    myMap.on('load', onMapLoad);
+    myMap.setView([48.087, -1.66], 15);
+
+    myMap.on('click', function(e){
+        let pos = e.latlng;
+        marker.setLatLng(pos);
+        zoomTo(this, pos);
+        position(pos);
+    });
+
+    L.DomEvent.on(marker, 'dragend', () => {
+        pos = marker.getLatLng();
+        position(pos);
+    });
+
+    // Btns "modif"
+    let btnMdif = document.querySelectorAll('a[href*=supprimer]');
+    btnMdif.forEach(b => {
+        b.addEventListener('click', e => {
+            e.preventDefault();
+            let destination = e.target.href;
+            dialogue.querySelector('span').textContent = e.target.dataset.name;
+            dialogue.style.setProperty('display', 'block');
+            reponseDialogue()
+                .then(function(e){
+                    window.location = destination;
+                })
+                .catch(function(){
+                    console.log("La réponse était 'Non'");
+                })
+                .then(function(){
+                    dialogue.style.setProperty('display', 'none');
+                })
+        })
+    })
+});
